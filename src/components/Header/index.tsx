fb35e424b814bac9dedd162773b5e3230dd546d4
kennycha <dudqn136@naver.com>
2023-08-14 12:05:47 +0900

    feat: view mode Header 스타일 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -27,41 +27,39 @@ const Header = () => {
   return (
     // 실제 Google Form에서는 View 모드에 Header가 없지만, 모드 변경 버튼을 위치시키기 위해 유지했습니다.
     <header className={cx("container", { view: isViewMode })}>
-      <div className={cx("logoWrapper")}>
-        <img className={cx("logo")} src={Logo} />
-      </div>
-      <div className={cx("menus")}>
-        {isViewMode ? (
-          <button className={cx("back")} onClick={onBackButtonClick}>
-            <Icon type={"back"} />
-          </button>
-        ) : (
-          <button className={cx("preview")} onClick={onPreviewButtonClick}>
-            <Icon type={"eye"} />
-          </button>
-        )}
-        {/* 아래 요소는 구현 외 요소라 기능은 구현하지 않습니다.  */}
-        {!isViewMode && (
-          <button className={cx("profile")}>
-            <p className={cx("profileText")}>영부</p>
-          </button>
-        )}
-      </div>
-      <div className={cx("title")}>{formTitle}</div>
-      {/* 아래 영역은 구현 외 영역이라 기능은 구현하지 않습니다.  */}
-      {!isViewMode && (
-        <div className={cx("tabs")}>
-          <div className={cx("tab", { current: true })}>
-            <p className={cx("tabText")}>질문</p>
-            <div className={cx("tabBottom")} />
+      {isViewMode ? (
+        <button className={cx("back")} onClick={onBackButtonClick}>
+          <Icon type={"back"} />
+        </button>
+      ) : (
+        <>
+          <div className={cx("logoWrapper")}>
+            <img className={cx("logo")} src={Logo} />
           </div>
-          <div className={cx("tab")}>
-            <p className={cx("tabText")}>응답</p>
+          <div className={cx("menus")}>
+            <button className={cx("preview")} onClick={onPreviewButtonClick}>
+              <Icon type={"eye"} />
+            </button>
+            {/* 아래 요소는 구현 외 요소라 기능은 구현하지 않습니다.  */}
+            <button className={cx("profile")}>
+              <p className={cx("profileText")}>영부</p>
+            </button>
           </div>
-          <div className={cx("tab")}>
-            <p className={cx("tabText")}>설정</p>
+          <div className={cx("title")}>{formTitle}</div>
+          {/* 아래 영역은 구현 외 영역이라 기능은 구현하지 않습니다.  */}
+          <div className={cx("tabs")}>
+            <div className={cx("tab", { current: true })}>
+              <p className={cx("tabText")}>질문</p>
+              <div className={cx("tabBottom")} />
+            </div>
+            <div className={cx("tab")}>
+              <p className={cx("tabText")}>응답</p>
+            </div>
+            <div className={cx("tab")}>
+              <p className={cx("tabText")}>설정</p>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </header>
   );
